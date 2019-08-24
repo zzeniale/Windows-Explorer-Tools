7327d1026639d6d84f86bf14fa45ebfb17b2e813
Elaine <llemonthyme@Mini.local>
2019-08-24 16:52:49 +0200

    First commit

---
+++ CopyFilesFromFolders.py
@@ -0,0 +1,37 @@
+"""
+
+Copies files from many folders
+
+
+"""
+import os
+import shutil
+import tkinter as tk
+from tkinter import filedialog, simpledialog
+import ctypes
+
+application_window = tk.Tk()
+
+# Ask the user to select a folder
+src = filedialog.askdirectory(parent=application_window,
+                                 initialdir=os.getcwd(),
+                                 title="Please select the source folder containing all the colony folders.")
+site = simpledialog.askstring("Input", "What is the site name? (e.g. LNR02, SIS01)",
+                                parent=application_window)
+application_window.destroy()
+
+if not os.path.exists(os.path.join(src,"For report")):
+    dest = os.makedirs(os.path.join(src,"For report"))
+
+for folder in os.listdir(src): 
+    if folder.startswith(site):
+        folderpath = os.path.join(src,folder)
+        if not len(os.listdir(folderpath)) == 0: 
+            #copies last image in each subfolder
+            img = os.listdir(folderpath)[-1] 
+            imgpath = os.path.join(folderpath,img)
+            imgcopy = shutil.copy(imgpath, os.path.join(src,"For report"))
+            newname = os.path.join(src,"For report",folder+'.jpg')
+            os.rename(imgcopy,newname)
+            
+ctypes.windll.user32.MessageBoxW(0, "All done!", "Message", 1)
---
+++ CreateFolders.py
@@ -0,0 +1,24 @@
+"""
+Copies files from many folders
+"""
+import os
+import tkinter as tk
+from tkinter import filedialog, simpledialog
+
+application_window = tk.Tk()
+
+# Ask the user to select a folder
+src = filedialog.askdirectory(parent=application_window,
+                              initialdir=os.getcwd(),
+                              title="Please select the destination folder.")
+n_colonies = simpledialog.askinteger("Input", 
+                                     "How many colonies are there in total?",
+                                     parent=application_window,
+                                     minvalue = 0)
+suffix = simpledialog.askstring("Input", "What is the folder suffix? E.g. SIS01-T, LNR02-N",
+                                parent=application_window)
+application_window.destroy()
+
+
+for foldername in range (1,n_colonies): 
+    os.mkdir(os.path.join(src,suffix+"-"+str(foldername))) #change the suffix to fit site codes
